3a7104542b353f5da296cd9c3e814deffddf6d4e
Andrew Komorkovich <komorkovics@gmail.com>
2023-01-28 22:07:05 +0200

    Clean up js code

--- drumkit.js
+++ drumkit.js
@@ -1,3 +1,5 @@
+const DRUMKIT_SELECTOR = '.drumkit__audio--';
+
 let buttonMap = {
     e: 'crash',
     r: 'ride',
@@ -11,15 +13,25 @@ let buttonMap = {
     b: 'kick'
 };
 
-function playAudio(event) {
-    if(typeof buttonMap[event.key] === 'undefined') return false;
-    let selector = '.drumkit__audio--' + buttonMap[event.key];
-    let drumsetPart = document.querySelector(selector);
 
-    if(typeof drumsetPart !== 'undefined') {
+function getEl(button) {
+    return document.querySelector(DRUMKIT_SELECTOR + button);
+}
+
+function playAudio(drumsetPart) {
+    if (typeof drumsetPart !== 'undefined') {
         drumsetPart.pause();
         drumsetPart.currentTime = 0;
         drumsetPart.play();
     }
 }
-document.addEventListener('keydown', playAudio);
+
+function buttonHandle(event) {
+    let button = buttonMap[event.key];
+    if (typeof button === 'undefined') return false;
+
+    let drumsetPart = getEl(button);
+    playAudio(drumsetPart);
+}
+
+document.addEventListener('keydown', buttonHandle);
