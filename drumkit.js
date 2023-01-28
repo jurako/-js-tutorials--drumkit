1e09c8e50a5e9501822b8fac9148f2207decf41b
Andrew Komorkovich <komorkovics@gmail.com>
2023-01-28 23:21:46 +0200

    Started work on button animations

--- drumkit.js
+++ drumkit.js
@@ -1,5 +1,6 @@
-const DRUMKIT_SELECTOR = '.drumkit__audio--';
-const DRUMKIT_ACTIVE_CLASS = 'drumkit__button--active';
+const DRUMKIT_AUDIO_SELECTOR = '.drumkit__audio--';
+const DRUMKIT_BUTTON_SELECTOR = '.drumkit__button-';
+const DRUMKIT_ACTIVE_CLASS = '-active';
 
 let buttonMap = {
     e: 'crash',
@@ -10,12 +11,14 @@ let buttonMap = {
     g: 'mid-tom',
     h: 'high-tom',
     j: 'snare',
-    v: 'kick',
-    b: 'kick'
+    v: 'left-kick',
+    b: 'right-kick'
 };
 
 
 function getEl(selector) {
+
+    console.log(selector);
     return document.querySelector(selector);
 }
 
@@ -29,17 +32,22 @@ function playAudio(drumkitPart) {
 
 function scale(element) {
     console.log(element);
-    console.log(element.querySelector('kbd'));
-    // element.querySelector('kbd').classList.add(DRUMKIT_ACTIVE_CLASS);
+    if (element !== null) {
+        let active = DRUMKIT_BUTTON_SELECTOR + DRUMKIT_ACTIVE_CLASS;
+        //scaling happens in CSS transition property
+        element.classList.add(active);
+        element.addEventListener('ontransitionend', function () {
+            this.classList.remove(active);
+        });
+    }
 }
 
 function buttonHandle(event) {
     let button = buttonMap[event.key];
     if (typeof button === 'undefined') return false;
 
-    let drumkitPart = ;
-    playAudio(getEl(DRUMKIT_SELECTOR + button));
-    scale(getEl);
+    playAudio(getEl(DRUMKIT_AUDIO_SELECTOR + button));
+    scale(getEl(DRUMKIT_BUTTON_SELECTOR + button));
 }
 
 document.addEventListener('keydown', buttonHandle);
