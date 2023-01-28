808eae0ec40c39104fe75e827323ee18e1cf7e64
Andrew Komorkovich <komorkovics@gmail.com>
2023-01-28 22:55:03 +0200

    Broken comit

--- drumkit.js
+++ drumkit.js
@@ -1,4 +1,5 @@
 const DRUMKIT_SELECTOR = '.drumkit__audio--';
+const DRUMKIT_ACTIVE_CLASS = 'drumkit__button--active';
 
 let buttonMap = {
     e: 'crash',
@@ -14,24 +15,31 @@ let buttonMap = {
 };
 
 
-function getEl(button) {
-    return document.querySelector(DRUMKIT_SELECTOR + button);
+function getEl(selector) {
+    return document.querySelector(selector);
 }
 
-function playAudio(drumsetPart) {
-    if (typeof drumsetPart !== 'undefined') {
-        drumsetPart.pause();
-        drumsetPart.currentTime = 0;
-        drumsetPart.play();
+function playAudio(drumkitPart) {
+    if (typeof drumkitPart !== 'undefined') {
+        drumkitPart.pause();
+        drumkitPart.currentTime = 0;
+        drumkitPart.play();
     }
+}
+
+function scale(element) {
+    console.log(element);
+    console.log(element.querySelector('kbd'));
+    // element.querySelector('kbd').classList.add(DRUMKIT_ACTIVE_CLASS);
 }
 
 function buttonHandle(event) {
     let button = buttonMap[event.key];
     if (typeof button === 'undefined') return false;
 
-    let drumsetPart = getEl(button);
-    playAudio(drumsetPart);
+    let drumkitPart = ;
+    playAudio(getEl(DRUMKIT_SELECTOR + button));
+    scale(getEl);
 }
 
 document.addEventListener('keydown', buttonHandle);
