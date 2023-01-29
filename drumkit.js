50cc4321e90b9b9287c36aaee707ca1027b7c2ba
Juris <juris@norgate.lv>
2023-01-29 12:40:31 +0200

    Finish button animations

--- drumkit.js
+++ drumkit.js
@@ -1,12 +1,12 @@
 const DRUMKIT_AUDIO_SELECTOR = '.drumkit__audio--';
 const DRUMKIT_BUTTON_SELECTOR = '.drumkit__button-';
-const DRUMKIT_ACTIVE_CLASS = '-active';
+const DRUMKIT_ACTIVE_CLASS = 'drumkit__button--active';
 
 let buttonMap = {
     e: 'crash',
     r: 'ride',
-    i: 'open-hihat',
-    k: 'closed-hihat',
+    i: 'hihat-open',
+    k: 'hihat-closed',
     f: 'floor-tom',
     g: 'mid-tom',
     h: 'high-tom',
@@ -17,27 +17,22 @@ let buttonMap = {
 
 
 function getEl(selector) {
-
-    console.log(selector);
     return document.querySelector(selector);
 }
 
 function playAudio(drumkitPart) {
     if (typeof drumkitPart !== 'undefined') {
-        drumkitPart.pause();
+        // drumkitPart.pause();
         drumkitPart.currentTime = 0;
         drumkitPart.play();
     }
 }
 
 function scale(element) {
-    console.log(element);
     if (element !== null) {
-        let active = DRUMKIT_BUTTON_SELECTOR + DRUMKIT_ACTIVE_CLASS;
-        //scaling happens in CSS transition property
-        element.classList.add(active);
-        element.addEventListener('ontransitionend', function () {
-            this.classList.remove(active);
+        element.classList.add(DRUMKIT_ACTIVE_CLASS);
+        element.addEventListener('transitionend', function () {
+            this.classList.remove(DRUMKIT_ACTIVE_CLASS);
         });
     }
 }
